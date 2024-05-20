f8d9c32cf98cf2b94b06d69a253347f69272055b
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-05-20 09:49:29 +0200

    added logger

--- Livekod/livekod/server.js
+++ Livekod/livekod/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import loggerMiddleware from './middlewares/logger.js';
 import errorHandlerMiddleware from './middlewares/errorHandler.js';
 const app = express();
 const PORT = 8080;
 
+// Middlewares
 app.use(express.json());
+app.use(loggerMiddleware);
 
 app.get('/error', (req, res, next) => {
     const error = new Error('Page not found!');
@@ -15,4 +18,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+// Middleware som måste ligga längst ner i dokumentet för att fånga upp fel
 app.use(errorHandlerMiddleware);
