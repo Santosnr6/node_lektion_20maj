63e71a0434acb2f515ef275487eeca0c2927859b
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-05-24 10:51:36 +0200

    lagt till video

--- Livekod/Movie-API/routes/auth.js
+++ Livekod/Movie-API/routes/auth.js
@@ -1,16 +1,18 @@
 import { Router } from 'express';
-import { users }  from '../config/data.js';
-import userSchema from '../models/userSchema.js';
+import { users } from '../config/data.js';
+import userSchema from '../models/userModel.js';
 
 const router = Router();
 
+// POST request to login
 router.post('/login', (req, res) => {
     const { error } = userSchema.validate(req.body);
-    if(error){
+
+    if(error) {
         const response = {
-            success: false,
-            message: error.details[0].message,
-            status: 400
+            success : false,
+            message : error.details[0].message,
+            status : 400
         }
         return res.status(400).json(response);
     }
@@ -19,24 +21,27 @@ router.post('/login', (req, res) => {
     const user = users.find(user => user.username === username && user.password === password);
     if(user) {
         global.currentUser = user;
-        res.json({ message : 'Login successful', user : user });
+        res.json({ message : 'Login successful', user : user})
     } else {
-        res.status(401).json({ message : 'Invalid credentials' });
+        res.status(401).json({ message : 'Invalid credentials'});
     }
 });
 
+
+// POST request to logout
 router.post('/logout', (req, res) => {
     global.currentUser = null;
-    res.json({ message : 'Logged out successfully' });
-});
+    res.json({ message : 'Logout successful' });
+})
 
+// POST request to register
 router.post('/register', (req, res) => {
     const { error } = userSchema.validate(req.body);
-    if(error){
+    if(error) {
         const response = {
-            success: false,
-            message: error.details[0].message,
-            status: 400
+            success : false,
+            message : error.details[0].message,
+            status : 400
         }
         return res.status(400).json(response);
     }
@@ -47,7 +52,7 @@ router.post('/register', (req, res) => {
         res.status(400).json({ message : 'User already exists' });
     } else {
         users.push({
-            id : users[users.length - 1].id + 1,
+            id: users[users.length - 1] + 1,
             username : username,
             password : password
         });
